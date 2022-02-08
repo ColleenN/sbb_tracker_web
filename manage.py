86b8d7e5f5de81293bb3a516a96637f3409b9578
CNelson <colleenfnelson@gmail.com>
2022-02-08 00:45:05 -0800

    Re-named directory main site config lives in because the django default is confusing af, imo.

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sbb_tracker.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'site_setup.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- site_setup/urls.py
+++ site_setup/urls.py
@@ -1,4 +1,4 @@
-"""sbb_tracker URL Configuration
+"""site_setup URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
