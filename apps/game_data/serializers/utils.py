7a1919ca69c6b148a506d87010b6517e62724328
CNelson <colleenfnelson@gmail.com>
2022-02-13 16:57:44 -0800

    Correct issue with JSONDashConverter, groundwork for ID Key JSON helper ListSerializer class.

--- apps/game_data/serializers/utils.py
+++ apps/game_data/serializers/utils.py
@@ -1,6 +1,12 @@
 """
 Serializer-related utility code not linked to a specific serializer.
 """
+from collections.abc import Mapping
+
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from rest_framework.settings import api_settings
+from rest_framework.utils import html
 
 
 class JSONDashConvertMixin:
@@ -25,8 +31,78 @@ class JSONDashConvertMixin:
     def to_representation(self, instance):
         """Back-convert underscores in outgoing data."""
         initial_repr = super().to_representation(instance)
+        new_dict = {}
         for field_name in initial_repr:
             if '_' in field_name:
-                actual = field_name.replace('_', '-')
-                initial_repr[actual] = initial_repr.pop(field_name)
-        return initial_repr
+                new_name = field_name.replace('_', '-')
+            else:
+                new_name = field_name
+            new_dict[new_name] = initial_repr[field_name]
+        return new_dict
+
+
+class IDKeyListSerializer(serializers.ListSerializer):
+    """
+    For cleanly handling JSON where object IDs are used as keys in what is
+    otherwise a 'flat' object listing.
+
+    ID key will be fed to child class via context in all child operations.
+    """
+
+    def get_initial(self):
+        if hasattr(self, 'initial_data'):
+            return self.to_representation(self.initial_data)
+        return {}
+
+    def to_internal_value(self, data):
+        """
+        List of dicts of native values <- List of dicts of primitive datatypes.
+        """
+        if html.is_html_input(data):
+            data = html.parse_html_list(data, default={})
+
+        if not isinstance(data, Mapping):
+            message = self.error_messages['invalid'].format(
+                datatype=type(data).__name__
+            )
+            raise ValidationError({
+                api_settings.NON_FIELD_ERRORS_KEY: [message]
+            }, code='invalid')
+
+        if not self.allow_empty and len(data) == 0:
+            message = self.error_messages['empty']
+            raise ValidationError({
+                api_settings.NON_FIELD_ERRORS_KEY: [message]
+            }, code='empty')
+
+        if self.max_length is not None and len(data) > self.max_length:
+            message = self.error_messages['max_length'].format(
+                max_length=self.max_length)
+            raise ValidationError({
+                api_settings.NON_FIELD_ERRORS_KEY: [message]
+            }, code='max_length')
+
+        if self.min_length is not None and len(data) < self.min_length:
+            message = self.error_messages['min_length'].format(
+                min_length=self.min_length)
+            raise ValidationError({
+                api_settings.NON_FIELD_ERRORS_KEY: [message]
+            }, code='min_length')
+
+        ret = []
+        errors = []
+
+        for key in data:
+            self.child.context['key'] = data[key]
+            try:
+                validated = self.child.run_validation(data[key])
+            except ValidationError as exc:
+                errors.append(exc.detail)
+            else:
+                ret.append(validated)
+                errors.append({})
+
+        if any(errors):
+            raise ValidationError(errors)
+
+        return ret
--- apps/game_data/tests/test_serializer_utils.py
+++ apps/game_data/tests/test_serializer_utils.py
@@ -1,21 +1,24 @@
-import json
-
 from django.test import TestCase
 from rest_framework import serializers
 
-from apps.game_data.serializers.utils import JSONDashConvertMixin
+from apps.game_data.serializers.utils import (
+    IDKeyListSerializer,
+    JSONDashConvertMixin
+)
 
 
 class SampleObject:
 
-    def __init__(self, field_val):
-        self.sample_field = field_val
+    def __init__(self, field_val_1, field_val_2):
+        self.sample_field_1 = field_val_1
+        self.sample_field_2 = field_val_2
 
 
 class SampleSerializer(JSONDashConvertMixin, serializers.Serializer):
-    """Dummy serializer for testing."""
+    """Dummy serializer for testing `JSONDashConvertMixin`."""
 
-    sample_field = serializers.CharField()
+    sample_field_1 = serializers.CharField()
+    sample_field_2 = serializers.CharField()
 
 
 class TestDashConvertMixin(TestCase):
@@ -25,8 +28,11 @@ class TestDashConvertMixin(TestCase):
 
     def setUp(self) -> None:
 
-        self.json = {"sample-field": "value"}
-        self.object = SampleObject("value")
+        self.json = {
+            "sample-field-1": "value1",
+            "sample-field-2": "value2"
+        }
+        self.object = SampleObject("value1", "value2")
 
     def test_representation(self):
         """Test that representation has dash in key name."""
@@ -39,5 +45,31 @@ class TestDashConvertMixin(TestCase):
         self.assertTrue(serializer.is_valid())
         self.assertEqual(
             serializer.validated_data,
-            {"sample_field": "value"}
+            {
+                "sample_field_1": "value1",
+                "sample_field_2": "value2"
+            }
         )
+
+
+class SampleIDKeyChildSerializer(serializers.Serializer):
+
+    sample_field = serializers.CharField()
+
+    class Meta:
+        list_serializer_class = IDKeyListSerializer
+
+
+class TestIDKeyChildSerializer(TestCase):
+    """
+    Tests for `IDKeyChildSerializer`.
+    """
+
+    def setUp(self) -> None:
+
+        self.json = {
+            "7": {'sample-field-2': 'value-a'},
+            "8": {'sample-field-2': 'value-b'}
+        }
+        self.object_1 = SampleObject("7", "value-a")
+        self.object_2 = SampleObject("8", "value-b")
