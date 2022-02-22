6e6cb3e2bd08c4a60035bf26470032ff0181b742
CNelson <colleenfnelson@gmail.com>
2022-02-21 20:06:12 -0800

    Add validator for strings describing XP values.

--- apps/game_data/serializers/validators.py
+++ apps/game_data/serializers/validators.py
@@ -4,6 +4,29 @@ Custom validators for our serializers.
 from rest_framework.serializers import ValidationError
 
 
+class XPStringValidator:
+    """
+    We're going to check that string describes a valid XP value, meaning:
+    1. Its of the format <X>.<Y>
+    2. The 1st value is an integer
+    3. The 2nd value is a 0, 1 or 2.
+    """
+
+    def __call__(self, value):
+
+        level, fraction = value.split('.')
+        try:
+            int(level)
+        except ValueError:
+            raise ValidationError(f'Level token {level} not an integer.')
+
+        if fraction not in ['0', '1', '2']:
+            raise ValidationError(
+                f'XP fraction token {fraction} not a valid partial XP value.')
+
+        return value
+
+
 class ValidateRootJSONPlayer:
     """
     Verifies that the player id in the root level of our JSON shows up in the
