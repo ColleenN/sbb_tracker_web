18ef98ab39f21cefb2e0f1aacccc36ffb41fc61e
colleen.nelson <colleen.nelson@healthvana.com>
2022-04-03 09:03:58 -0600

    Add some __init__ files

--- apps/game_data/apps.py
+++ apps/game_data/apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 
 class GameDataConfig(AppConfig):
     name = 'apps.game_data'
+    module = 'apps.game_data'
+    label = 'game_data'
+
+    def ready(self):
+
+        print("I LOADED")
---
+++ site_setup/temp/settings/__init__.py
@@ -0,0 +1,2 @@
+from site_setup.temp.settings import base_settings
+#include('base_settings.py', optional('.local_settings.py'))
