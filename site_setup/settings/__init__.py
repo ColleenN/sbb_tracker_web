19875dab224c823bddeccee6f36bb6e1c1d45a1f
colleen.nelson <colleen.nelson@healthvana.com>
2022-04-03 11:05:27 -0600

    Split settings finally working huzzah

---
+++ site_setup/settings/__init__.py
@@ -0,0 +1,2 @@
+from split_settings.tools import include, optional
+include('base_settings.py', optional('.local_settings.py'))
--- site_setup/temp/settings/__init__.py
+++
@@ -1,2 +0,0 @@
-from site_setup.temp.settings import base_settings
-#include('base_settings.py', optional('.local_settings.py'))
